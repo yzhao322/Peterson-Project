7bf5a15fd2055b4556bba63bb1d7704ac9f229e5
Eric Zeng <38875467+xzengeric@users.noreply.github.com>
2020-05-14 17:47:07 -0700

    Login/signup components
    Login/signup components.
    Working connection to db
    passport, bcrypt, etc

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
+
 import Home from "./pages/Home";
 import Detail from "./pages/Detail";
 import NoMatch from "./pages/NoMatch";
@@ -7,27 +9,28 @@ import Navbars from "./components/Nav";
 import Footer from "./components/Footer";
 import FavoritesList from "./pages/FavoritesList";
 import ShoppingCart from "./pages/ShoppingCart";
-import Login from "./pages/Login";
+import LoginIndex from "./components/Login/index";
 import Product from "./pages/ProductList";
 import ShopingCart from "./pages/shopping-cart";
 
 function App() {
+  
   return (
     <Router>
-      <div>
-          <Navbars />
-          <Switch>
-            <Route exact path="/" component={Home} />
-            <Route exact path="/home" component={Home} />
-            <Route exact path="/Login" component={Login} />
+      <>
+        <Navbars />
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/home" component={Home} />
+          <Route exact path="/Login" component={LoginIndex} />
           <Route exact path="/product" component={Product} />
           <Route exact path="/shopping-cart" component={ShopingCart} />
-            <Route exact path="/favorites" component={FavoritesList} />
-            <Route exact path="/posts/:id" component={Detail} />
-            <Route component={NoMatch} />
+          <Route exact path="/favorites" component={FavoritesList} />
+          <Route exact path="/posts/:id" component={Detail} />
+          <Route component={NoMatch} />
         </Switch>
         <Footer />
-      </div>
+      </>
     </Router>
   );
 }
--- models/user.js
+++ models/user.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
+var bcrypt = require("bcryptjs");
 
 const UserSchema = new Schema({
   username: {
@@ -9,8 +10,22 @@ const UserSchema = new Schema({
   },
   password: {
     type: String,
+    allowNull: false,
     required: true,
   },
+});
+
+
+
+UserSchema.prototype.valPassword = function (password) {
+  console.log(bcrypt.compareSync(password, this.password));
+  return bcrypt.compareSync(password, this.password);
+}
+
+// Hooks are automatic methods that run during various phases of the User Model lifecycle
+// In this case, before a User is created, we will automatically hash their password
+UserSchema.addHook("beforeCreate", function (user) {
+  UserSchema.password = bcrypt.hashSync(Schema.password, bcrypt.genSaltSync(10), null);
 });
 
 module.exports = Produce = mongoose.model("user", UserSchema);
