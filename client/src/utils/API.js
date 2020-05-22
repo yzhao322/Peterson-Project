485eff3223006f16ae7804941f0f962f8513c338
Alex Bilba <alex.shanless@gmail.com>
2020-05-21 18:22:19 -0700

    commit

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -3,13 +3,12 @@ import axios from "axios";
 export default {
   postProduce: (data) => axios.post("/api/produces", data),
   // getProduces: () => axios.get("/api/produces"),
+  removeproduce: (id) => axios.delete("/api/produces" + id),
 
   postUser: (data) => axios.post("/api/users", data),
 
   //login post
   postLogin: (data) => axios.post("/api/login", data),
-
-
 
   getProduce: () => axios.get("/api/produces"),
 };
