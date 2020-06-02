8c91ab638bb8f442a8b839846a6acd5ea76363a4
Gil Solis <gilsolisuw@gmail.com>
2020-06-02 12:41:27 -0700

    heroku

--- server.js
+++ server.js
@@ -1,30 +1,27 @@
 const express = require("express");
 const session = require("express-session");
-
 const passport = require("passport");
-
-
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
-
 const produces = require("./routes/api/produces");
 const order = require("./routes/api/order");
 const users = require("./routes/api/users");
 const orders = require("./routes/api/orders");
 const orderhistory = require("./routes/api/orderhistory")
-
-
 const app = express();
 // Creating express app and configuring middleware needed for authentication
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-app.use(express.static("public"));
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
+// app.use(express.static("public"));
 // We need to use sessions to keep track of our user's login status
 app.use(session({ secret: "keyboard cat", resave: true, saveUninitialized: true }));
 app.use(passport.initialize());
 app.use(passport.session());
-
 
 app.use(bodyParser.json());
 
@@ -32,7 +29,6 @@ app.use("/api/orderhistory", orderhistory);
 app.use("/api/produces", produces);
 app.use("/api/orders", orders);
 app.use("/api/users", users);
-
 
 // require("./routes/api/users")(app);
 // require("./routes/html-routes.js")(app);
